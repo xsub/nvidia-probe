304baba2f17a2861e900c8ee8ba4708764952d65
Pawel Suchanecki <psuchanecki@almalinux.org>
2023-05-30 18:13:06 +0200

    updated formatting of output

--- nvida-probe.sh
+++ nvida-probe.sh
@@ -2,14 +2,17 @@
 # The AlmaLinux NVIDIA compatiblity matrix report generator (script).
 
 separator() {
-    printf '%*s\n' "${COLUMNS:-$(tput cols)}" '' | tr ' ' '-'
+    printf '%*s\n' "${COLUMNS:-$(tput cols)}" | tr ' ' $1
 }
 
 sep_print() {
-    separator
+    separator -
     echo $*
-    separator 
+    separator =
 }
+
+sep_print "TIMESTAMP"
+date +%Y-%m-%d-%H-%M-%S
 
 # Find kernel settings
 sep_print "KERNEL VERSION"
@@ -26,16 +29,23 @@ grep GRUB_CMDLINE_LINUX /etc/default/grub
 sep_print "PACKAGES"
 sudo dnf list installed '*nvidia*'
 
+# Find Display-class devicess
+sep_print "DISPLAY CLASSH HW LIST (lshw)"
+sudo lshw -class display
+
+# List NVIDIA devices
+sep_print "NVIDIA DEVICES LIST (lspci)"
+lspci -kvd 10de:
+
 # Find interesting data in /dev/log/Xorg.0.log
-sep_print "XORG LOG DETAILS"
-[ -f /var/log/Xorg.0.log ] && 
+sep_print "XORG LOG DETAILS (parse for selected)"
+[ -f /var/log/Xorg.0.log ] &&
 grep -E 'X.Org X |loading driver|Kernel command|Operating|NVIDIA GLX Module' /var/log/Xorg.0.log | \
-cut -d']' -f2- | sed 's/^ //' | tr -d "\t" 
+cut -d']' -f2- | sed 's/^ //' | tr -d "\t"
 
 # Find GLX renderer info 
-
-
-if w $USER | grep -q ":0"; then 
+if w $USER | grep -q ":0"; then
   sep_print "GLXGEARS INFO"
-  timeout 3 glxgears -info }
+  timeout 5 glxgears -info
 fi
+
